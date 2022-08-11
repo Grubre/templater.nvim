06aa9ad8d080d69c01454f22319dfd93765ad615
Grubre <ogrodowczykjakub@gmail.com>
2022-08-11 23:15:14 +0200

    feat: Added file_templater with basic structure for a module

---
+++ lua/templater/file_templater.lua
@@ -0,0 +1,14 @@
+local file_templater = {}
+
+file_templater.add_template = function()
+end
+
+file_templater.copy_template = function ()
+    
+end
+
+file_templater.remove_template = function ()
+    
+end
+
+return file_templater
--- lua/templater/init.lua
+++ lua/templater/init.lua
@@ -6,4 +6,4 @@ local templator = {}
 templator.setup = config.setup
 templator.sub = substitutor._substitute_vars
 
-return templator 
+return templator
