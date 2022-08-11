e6dd861a38ce9a9f24666270209bc7d64bd2061d
Grubre <ogrodowczykjakub@gmail.com>
2022-08-11 15:53:16 +0200

    fix: Regex now only substitutes exact matches

--- lua/templator/var_substitutor.lua
+++ lua/templator/var_substitutor.lua
@@ -5,8 +5,9 @@ M._substitute_vars = function ()
     -- Get the current cursor position to restore it after the substitutions
     local og_row, og_column = unpack(vim.api.nvim_win_get_cursor(0))
 
+    -- Substitute the variables
     for key,value in pairs(config.options.variables) do
-        vim.api.nvim_command('silent! %s:'..key..':'..value..':g')
+        vim.api.nvim_command('silent! %s:\\<'..key..'\\>:'..value..':g')
     end
 
     -- Move the cursor back to it's original position
