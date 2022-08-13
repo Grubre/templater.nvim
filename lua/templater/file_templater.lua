53d4e12e50fe76b0851d83faade292ee38898fb4
Grubre <ogrodowczykjakub@gmail.com>
2022-08-13 17:17:41 +0200

    feat: Added a way to delete already created templates

--- lua/templater/file_templater.lua
+++ lua/templater/file_templater.lua
@@ -1,20 +1,28 @@
 local file_path = require("templater.config").options.file_templates_path
 local file_templater = {}
 
-file_templater.add_template = function()
-    vim.ui.input({prompt = "Name of the template"}, function(input)
-        vim.cmd('w '..file_path..input)
-    end)
-    -- print(file_path)
-    -- local file = io.open(file_path..file_name, "w")
+file_templater.add_template = function(file_name)
+    if file_name==nil then
+        vim.ui.input({prompt = "Name of the template"}, function(input)
+            vim.cmd('w '..file_path..input)
+        end)
+    else
+        vim.cmd('w '..file_path..file_name)
+    end
 end
 
 file_templater.copy_template = function ()
     
 end
 
-file_templater.remove_template = function ()
-    
+file_templater.remove_template = function (file_name)
+    if file_name==nil then
+        vim.ui.input({prompt = "Name of the template to delete"}, function(input)
+            vim.cmd('!rm '..file_path..input)
+        end)
+    else
+        vim.cmd('!rm '..file_path..file_name)
+    end
 end
 
 return file_templater
