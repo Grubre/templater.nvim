ae9dc8efdd4532bd454e2a255e8577683f32f478
Grubre <ogrodowczykjakub@gmail.com>
2022-08-13 17:03:46 +0200

    feat: Added a way to save templates

--- lua/templater/config.lua
+++ lua/templater/config.lua
@@ -1,11 +1,15 @@
 local config = {}
 
 local defaults = {
-    variables = {}
+    file_templates_path = vim.fn.stdpath("data").."/templater/",
+    variables = {},
 }
 
 config.setup = function(opts)
     config.options = vim.tbl_deep_extend("force", {}, defaults, opts or {})
+    if vim.fn.isdirectory(defaults.file_templates_path)==0 then
+        vim.fn.mkdir(defaults.file_templates_path, "")
+    end
 end
 
 return config
--- lua/templater/file_templater.lua
+++ lua/templater/file_templater.lua
@@ -1,6 +1,12 @@
+local file_path = require("templater.config").options.file_templates_path
 local file_templater = {}
 
 file_templater.add_template = function()
+    vim.ui.input({prompt = "Name of the template"}, function(input)
+        vim.cmd('w '..file_path..input)
+    end)
+    -- print(file_path)
+    -- local file = io.open(file_path..file_name, "w")
 end
 
 file_templater.copy_template = function ()
