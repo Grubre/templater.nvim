742374182d96eb3056a42658c748812269c80b55
Grubre <ogrodowczykjakub@gmail.com>
2022-08-13 23:21:52 +0200

    fix: Added escape characters for different brackets, hashes, etc.

--- lua/templater/var_substitutor.lua
+++ lua/templater/var_substitutor.lua
@@ -1,17 +1,31 @@
 local config = require("templater.config")
 local M = {}
 
+local escape_problematic_chars = function(str)
+    return str:gsub("%W", {
+        ["#"] = "\\#",
+        ["("] = "\\(",
+        [")"] = "\\)",
+        ["]"] = "\\]",
+        ["["] = "\\[",
+        ["{"] = "\\{",
+        ["}"] = "\\}",
+    })
+end
+
 M._substitute_vars = function ()
     -- Get the current cursor position to restore it after the substitutions
     local og_row, og_column = unpack(vim.api.nvim_win_get_cursor(0))
 
     -- Substitute the variables
     for pattern,func in pairs(config.options.variables) do
+        local escaped_pattern = escape_problematic_chars(pattern)
         if vim.fn.search(pattern)==0 then
             goto continue
         end
+        print(pattern)
         local callback_func = function(val)
-            vim.api.nvim_command('silent! %s:\\<'..pattern..'\\>:'..val..':g')
+            vim.api.nvim_command('silent! %s:\\<'..escaped_pattern..'\\>:'..val..':g')
         end
         func(callback_func)
         ::continue::
