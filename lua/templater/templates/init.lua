01465e4bf7faa64f7c878371c80d79264771eb06
Grubre <ogrodowczykjakub@gmail.com>
2022-08-13 13:06:19 +0200

    feat: Added a new template: input which uses vim.ui.input()

--- lua/templater/templates/init.lua
+++ lua/templater/templates/init.lua
@@ -6,4 +6,13 @@ M.string = function(str)
     end
 end
 
+M.input = function()
+    return function(callback)
+        local p = "Input"
+        vim.ui.input({prompt = p}, function(input)
+            callback(input)
+        end)
+    end
+end
+
 return M
